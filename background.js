ff5cad4ee76b6670a2df3adab152cf2f6d8e6b9d
jeffreyshen19 <jshen@andover.edu>
2018-09-08 17:51:31 -0400

    successfully sends to the server

--- background.js
+++ background.js
@@ -18,15 +18,13 @@ function logURL(requestDetails) {
     // Then POST to the server if it has the correct data
     if(responseText.indexOf("\\u6839\\u636e\\u76f8\\u5173\\u6cd5\\u5f8b\\u6cd5\\u89c4\\u548c\\u653f\\u7b56\\uff0c\\u201c") != -1 && responseText.indexOf("\\u201d\\u641c\\u7d22\\u7ed3\\u679c\\u672a\\u4e88\\u663e\\u793a") != -1){
       console.log("YOU HAVE POSTED CENSORED CONTENT");
-      // xhr = new XMLHttpRequest();
-      // xhr.open("POST", "https://cs.dpccdn.net/v1/", true);
-      // xhr.setRequestHeader('Content-Type', 'application/json');
-      // xhr.send(JSON.stringify({
-      //   text: postText
-      // }));
+      xhr = new XMLHttpRequest();
+      xhr.open("POST", "https://cs.dpccdn.net/v1", true);
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.send(JSON.stringify({
+        text: postText
+      }));
     }
-
-
   }
 }
 
